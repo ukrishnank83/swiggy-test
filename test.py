6b386a1efab062544879e0361b9e1a2269441081
unni <unni@unni>
2016-06-02 23:49:21 +0530

    Commenting

--- test.py
+++ test.py
@@ -1,16 +1,20 @@
 import fnmatch,os,json,logging, sys, datetime
+
 matches = []
-LOG_FILENAME = 'json.log'
+
+LOG_FILENAME = 'json.log'                                                #log file
 logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)
 
+error=0
 dir_name = sys.argv[1]
 now = datetime.datetime.now()
+
 for root, dirnames, filenames in os.walk(dir_name):
-    for filename in fnmatch.filter(filenames, '*.json'):
+    for filename in fnmatch.filter(filenames, '*.json'):                 #getting the names of all the json files
         matches.append(os.path.join(root, filename))
         pathname = root+ '/' + filename
         print pathname
-        with open(pathname) as json_file:
+        with open(pathname) as json_file:                                #json validations
              try:
                 json_data = json.load(json_file)
              except ValueError, e:
@@ -18,4 +22,11 @@ for root, dirnames, filenames in os.walk(dir_name):
                 logging.debug(now.strftime("%Y-%m-%d %H:%M")) 
                 logging.debug(pathname)
                 logging.debug(e)
-        
+                error +=1
+
+if error == 0:
+   print "All the json files are valid"
+else:
+   print "No Of Files with errors"
+   print error
+
